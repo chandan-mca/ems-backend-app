dc0b6ee27105c05b1213e63104d98b1cc8971c2e
Chandan Agrawal <chandan.acuver@gmail.com>
2023-12-09 22:07:01 +0530

    my first commit

---
+++ src/main/java/com/project/emsbackendapp/EmsBackendAppApplication.java
@@ -0,0 +1,13 @@
+package com.project.emsbackendapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmsBackendAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmsBackendAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/emsbackendapp/EmsBackendAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.emsbackendapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmsBackendAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
