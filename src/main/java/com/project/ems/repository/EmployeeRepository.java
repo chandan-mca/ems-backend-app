295bd2f549660709f85cbfbc17fc032ac42ac4ef
Chandan Agrawal <chandan.acuver@gmail.com>
2023-12-10 21:41:27 +0530

    Create EmployeeRepository

---
+++ src/main/java/com/project/ems/repository/EmployeeRepository.java
@@ -0,0 +1,7 @@
+package com.project.ems.repository;
+
+import com.project.ems.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+}
