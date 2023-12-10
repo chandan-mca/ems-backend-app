97cc8c89767e8f0a277dc96a17a984e1044fe58f
Chandan Agrawal <chandan.acuver@gmail.com>
2023-12-10 21:56:23 +0530

    Create EmployeeDto and EmployeeMapper

---
+++ src/main/java/com/project/ems/mapper/EmployeeMapper.java
@@ -0,0 +1,25 @@
+package com.project.ems.mapper;
+
+import com.project.ems.dto.EmployeeDto;
+import com.project.ems.entity.Employee;
+
+public class EmployeeMapper {
+
+    public static EmployeeDto mapToEmployeeDto(Employee employee){
+        return new EmployeeDto(
+                employee.getId(),
+                employee.getFirstName(),
+                employee.getLastName(),
+                employee.getEmail()
+        );
+    }
+
+    public static Employee mapToEmployee(EmployeeDto employeeDto){
+        return new Employee(
+                employeeDto.getId(),
+                employeeDto.getFirstName(),
+                employeeDto.getLastName(),
+                employeeDto.getEmail()
+        );
+    }
+}
